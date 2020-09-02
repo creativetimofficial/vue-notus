277028717604874bc30fb027cd164b3ca36d08a7
EINazare <emanuelioannazare@gmail.com>
2020-09-02 19:20:17 +0300

    feactor: trying to change the routes to use VueRouter

--- src/main.js
+++ src/main.js
@@ -1,13 +1,16 @@
 import Vue from "vue";
+import VueRouter from "vue-router";
 import Dashboard from "./views/Dashboard.vue";
 
 import "@fortawesome/fontawesome-free/css/all.min.css";
+import "./assets/styles/tailwind.css";
 
 Vue.config.productionTip = false;
 
-const routes = {
-  "/dashboard": Dashboard,
-};
+const routes = [
+  { path: '/dashboard', component: Dashboard },
+  { path: '*', redirect: '/dashboard' }
+];
 
 const router = new VueRouter({
   routes
