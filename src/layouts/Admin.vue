08bfc25c64f609310514c00f449c97ff7f3909b5
EINazare <emanuelioannazare@gmail.com>
2020-09-03 11:55:08 +0300

    refactor: change react code to vuejs

--- src/layouts/Admin.vue
+++ src/layouts/Admin.vue
@@ -1,38 +1,28 @@
-import React from "react";
-import { Switch, Route } from "react-router-dom";
-
-// components
-
-import AdminNavbar from "components/Navbars/AdminNavbar.js";
-import Sidebar from "components/Sidebar/Sidebar.js";
-import HeaderStats from "components/Headers/HeaderStats.js";
-import FooterAdmin from "components/Footers/FooterAdmin.js";
-
-// views
-
-import Dashboard from "views/admin/Dashboard.js";
-import Maps from "views/admin/Maps.js";
-import Settings from "views/admin/Settings.js";
-import Tables from "views/admin/Tables.js";
-
-export default function Admin() {
-  return (
-    <>
-      <Sidebar />
-      <div className="relative md:ml-64 bg-gray-200">
-        <AdminNavbar />
-        {/* Header */}
-        <HeaderStats />
-        <div className="px-4 md:px-10 mx-auto w-full -m-24">
-          <Switch>
-            <Route path="/admin/dashboard" component={Dashboard} />
-            <Route path="/admin/maps" component={Maps} />
-            <Route path="/admin/settings" component={Settings} />
-            <Route path="/admin/tables" component={Tables} />
-          </Switch>
-          <FooterAdmin />
-        </div>
+<template>
+  <div>
+    <sidebar />
+    <div class="relative md:ml-64 bg-gray-200">
+      <admin-navbar />
+      <header-stats />
+      <div class="px-4 md:px-10 mx-auto w-full -m-24">
+        <router-view />
+        <footer-admin />
       </div>
-    </>
-  );
-}
+    </div>
+  </div>
+</template>
+<script>
+import AdminNavbar from "@/components/Navbars/AdminNavbar.vue";
+import Sidebar from "@/components/Sidebar/Sidebar.vue";
+import HeaderStats from "@/components/Headers/HeaderStats.vue";
+import FooterAdmin from "@/components/Footers/FooterAdmin.vue";
+export default {
+  name: "admin-layout",
+  components: {
+    AdminNavbar,
+    Sidebar,
+    HeaderStats,
+    FooterAdmin,
+  },
+};
+</script>
