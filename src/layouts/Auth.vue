d6d194945ee3a23fbc7e6260ecdc4afbd0bf0e74
EINazare <emanuelioannazare@gmail.com>
2020-09-03 15:19:44 +0300

    fix: solve path import error

--- src/layouts/Auth.vue
+++ src/layouts/Auth.vue
@@ -15,7 +15,7 @@
 import Navbar from "@/components/Navbars/AuthNavbar.vue";
 import FooterSmall from "@/components/Footers/FooterSmall.vue";
 
-import registerBg2 from "assets/img/register_bg_2.png";
+import registerBg2 from "@/assets/img/register_bg_2.png";
 
 export default {
   components: {
