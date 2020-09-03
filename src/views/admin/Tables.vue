cb8da73f34e543aad9c79cc18aad3fb5d3c5f118
EINazare <emanuelioannazare@gmail.com>
2020-09-03 13:24:09 +0300

    refactor: change react code to vuejs

--- src/views/admin/Tables.vue
+++ src/views/admin/Tables.vue
@@ -1,20 +1,19 @@
-import React from "react";
-
-// components
-
+<template>
+  <div class="flex flex-wrap mt-4">
+    <div class="w-full mb-12 px-4">
+      <card-table />
+    </div>
+    <div class="w-full mb-12 px-4">
+      <card-table color="dark" />
+    </div>
+  </div>
+</template>
+<script>
 import CardTable from "@/components/Cards/CardTable.vue";
 
-export default function Tables() {
-  return (
-    <>
-      <div class="flex flex-wrap mt-4">
-        <div class="w-full mb-12 px-4">
-          <CardTable />
-        </div>
-        <div class="w-full mb-12 px-4">
-          <CardTable color="dark" />
-        </div>
-      </div>
-    </>
-  );
-}
+export default {
+  components: {
+    CardTable,
+  },
+};
+</script>
