fb2025e7f81ce020362f4d7c1ec960dce16b28ed
EINazare <emanuelioannazare@gmail.com>
2020-09-03 11:55:19 +0300

    refactor: change react code to vuejs

--- src/components/Footers/FooterAdmin.vue
+++ src/components/Footers/FooterAdmin.vue
@@ -1,62 +1,67 @@
-import React from "react";
-
-export default function FooterAdmin() {
-  return (
-    <>
-      <footer className="block py-4">
-        <div className="container mx-auto px-4">
-          <hr className="mb-4 border-b-1 border-gray-300" />
-          <div className="flex flex-wrap items-center md:justify-between justify-center">
-            <div className="w-full md:w-4/12 px-4">
-              <div className="text-sm text-gray-600 font-semibold py-1">
-                Copyright © {new Date().getFullYear()}{" "}
-                <a
-                  href="https://www.creative-tim.com"
-                  className="text-gray-600 hover:text-gray-800 text-sm font-semibold py-1"
-                >
-                  Creative Tim
-                </a>
-              </div>
-            </div>
-            <div className="w-full md:w-8/12 px-4">
-              <ul className="flex flex-wrap list-none md:justify-end  justify-center">
-                <li>
-                  <a
-                    href="https://www.creative-tim.com"
-                    className="text-gray-700 hover:text-gray-900 text-sm font-semibold block py-1 px-3"
-                  >
-                    Creative Tim
-                  </a>
-                </li>
-                <li>
-                  <a
-                    href="https://www.creative-tim.com/presentation"
-                    className="text-gray-700 hover:text-gray-900 text-sm font-semibold block py-1 px-3"
-                  >
-                    About Us
-                  </a>
-                </li>
-                <li>
-                  <a
-                    href="http://blog.creative-tim.com"
-                    className="text-gray-700 hover:text-gray-900 text-sm font-semibold block py-1 px-3"
-                  >
-                    Blog
-                  </a>
-                </li>
-                <li>
-                  <a
-                    href="https://github.com/creativetimofficial/tailwind-webapp-react/blob/master/LICENSE.md"
-                    className="text-gray-700 hover:text-gray-900 text-sm font-semibold block py-1 px-3"
-                  >
-                    MIT License
-                  </a>
-                </li>
-              </ul>
-            </div>
+<template>
+  <footer class="block py-4">
+    <div class="container mx-auto px-4">
+      <hr class="mb-4 border-b-1 border-gray-300" />
+      <div
+        class="flex flex-wrap items-center md:justify-between justify-center"
+      >
+        <div class="w-full md:w-4/12 px-4">
+          <div class="text-sm text-gray-600 font-semibold py-1">
+            Copyright © {{ date }}
+            <a
+              href="https://www.creative-tim.com"
+              class="text-gray-600 hover:text-gray-800 text-sm font-semibold py-1"
+            >
+              Creative Tim
+            </a>
           </div>
         </div>
-      </footer>
-    </>
-  );
-}
+        <div class="w-full md:w-8/12 px-4">
+          <ul class="flex flex-wrap list-none md:justify-end justify-center">
+            <li>
+              <a
+                href="https://www.creative-tim.com"
+                class="text-gray-700 hover:text-gray-900 text-sm font-semibold block py-1 px-3"
+              >
+                Creative Tim
+              </a>
+            </li>
+            <li>
+              <a
+                href="https://www.creative-tim.com/presentation"
+                class="text-gray-700 hover:text-gray-900 text-sm font-semibold block py-1 px-3"
+              >
+                About Us
+              </a>
+            </li>
+            <li>
+              <a
+                href="http://blog.creative-tim.com"
+                class="text-gray-700 hover:text-gray-900 text-sm font-semibold block py-1 px-3"
+              >
+                Blog
+              </a>
+            </li>
+            <li>
+              <a
+                href="https://github.com/creativetimofficial/tailwind-webapp-react/blob/master/LICENSE.md"
+                class="text-gray-700 hover:text-gray-900 text-sm font-semibold block py-1 px-3"
+              >
+                MIT License
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </footer>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      date: new Date().getFullYear(),
+    };
+  },
+};
+</script>
