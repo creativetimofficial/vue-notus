e7c5237ea089d8180c3368e4b80541f83576261a
EINazare <emanuelioannazare@gmail.com>
2020-09-03 14:59:45 +0300

    refactor: change react code to vuejs

--- src/components/Navbars/IndexNavbar.vue
+++ src/components/Navbars/IndexNavbar.vue
@@ -1,97 +1,112 @@
-import React from "react";
-import { Link } from "react-router-dom";
-// components
+<template>
+  <nav
+    class="top-0 fixed z-50 w-full flex flex-wrap items-center justify-between px-2 py-3 navbar-expand-lg bg-white shadow"
+  >
+    <div
+      class="container px-4 mx-auto flex flex-wrap items-center justify-between"
+    >
+      <div
+        class="w-full relative flex justify-between lg:w-auto lg:static lg:block lg:justify-start"
+      >
+        <Link to="/">
+          <a
+            class="text-gray-800 text-sm font-bold leading-relaxed inline-block mr-4 py-2 whitespace-no-wrap uppercase"
+            href="#pablo"
+          >
+            Tailwind Webapp NextJS
+          </a>
+        </Link>
+        <button
+          class="cursor-pointer text-xl leading-none px-3 py-1 border border-solid border-transparent rounded bg-transparent block lg:hidden outline-none focus:outline-none"
+          type="button"
+          v-on:click="setNavbarOpen"
+        >
+          <i class="text-white fas fa-bars"></i>
+        </button>
+      </div>
+      <div
+        class="lg:flex flex-grow items-center bg-white lg:bg-transparent lg:shadow-none"
+        :class="[navbarOpen ? 'block rounded shadow-lg' : 'hidden']"
+        id="example-navbar-warning"
+      >
+        <ul class="flex flex-col lg:flex-row list-none mr-auto">
+          <li class="flex items-center">
+            <a
+              class="hover:text-gray-600 text-gray-800 px-3 py-4 lg:py-2 flex items-center text-xs uppercase font-bold"
+              href="https://www.creative-tim.com/learning-lab/tailwind/overview/webapp-nextjs"
+            >
+              <i
+                class="text-gray-500 far fa-file-alt text-lg leading-lg mr-2"
+              />
+              Docs
+            </a>
+          </li>
+        </ul>
+        <ul class="flex flex-col lg:flex-row list-none lg:ml-auto">
+          <li class="flex items-center">
+            <index-dropdown />
+          </li>
+          <li class="flex items-center">
+            <a
+              class="hover:text-gray-600 text-gray-800 px-3 py-4 lg:py-2 flex items-center text-xs uppercase font-bold"
+              href="#pablo"
+            >
+              <i class="text-gray-500 fab fa-facebook text-lg leading-lg" />
+              <span class="lg:hidden inline-block ml-2">Share</span>
+            </a>
+          </li>
 
+          <li class="flex items-center">
+            <a
+              class="hover:text-gray-600 text-gray-800 px-3 py-4 lg:py-2 flex items-center text-xs uppercase font-bold"
+              href="#pablo"
+            >
+              <i class="text-gray-500 fab fa-twitter text-lg leading-lg" />
+              <span class="lg:hidden inline-block ml-2">Tweet</span>
+            </a>
+          </li>
+
+          <li class="flex items-center">
+            <a
+              class="hover:text-gray-600 text-gray-800 px-3 py-4 lg:py-2 flex items-center text-xs uppercase font-bold"
+              href="#pablo"
+            >
+              <i class="text-gray-500 fab fa-github text-lg leading-lg" />
+              <span class="lg:hidden inline-block ml-2">Star</span>
+            </a>
+          </li>
+
+          <li class="flex items-center">
+            <button
+              class="bg-blue-500 text-white active:bg-blue-600 text-xs font-bold uppercase px-4 py-2 rounded shadow hover:shadow-lg outline-none focus:outline-none lg:mr-1 lg:mb-0 ml-3 mb-3"
+              type="button"
+              style="transition: all 0.15s ease;"
+            >
+              <i class="fas fa-arrow-alt-circle-down"></i> Download
+            </button>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+</template>
+
+<script>
 import IndexDropdown from "@/components/Dropdowns/IndexDropdown.vue";
 
-export default function Navbar(props) {
-  const [navbarOpen, setNavbarOpen] = React.useState(false);
-  return (
-    <>
-      <nav class="top-0 fixed z-50 w-full flex flex-wrap items-center justify-between px-2 py-3 navbar-expand-lg bg-white shadow">
-        <div class="container px-4 mx-auto flex flex-wrap items-center justify-between">
-          <div class="w-full relative flex justify-between lg:w-auto lg:static lg:block lg:justify-start">
-            <Link to="/">
-              <a
-                class="text-gray-800 text-sm font-bold leading-relaxed inline-block mr-4 py-2 whitespace-no-wrap uppercase"
-                href="#pablo"
-              >
-                Tailwind Webapp NextJS
-              </a>
-            </Link>
-            <button
-              class="cursor-pointer text-xl leading-none px-3 py-1 border border-solid border-transparent rounded bg-transparent block lg:hidden outline-none focus:outline-none"
-              type="button"
-              onClick={() => setNavbarOpen(!navbarOpen)}
-            >
-              <i class="text-white fas fa-bars"></i>
-            </button>
-          </div>
-          <div
-            className={
-              "lg:flex flex-grow items-center bg-white lg:bg-transparent lg:shadow-none" +
-              (navbarOpen ? " block rounded shadow-lg" : " hidden")
-            }
-            id="example-navbar-warning"
-          >
-            <ul class="flex flex-col lg:flex-row list-none mr-auto">
-              <li class="flex items-center">
-                <a
-                  class="hover:text-gray-600 text-gray-800 px-3 py-4 lg:py-2 flex items-center text-xs uppercase font-bold"
-                  href="https://www.creative-tim.com/learning-lab/tailwind/overview/webapp-nextjs"
-                >
-                  <i class="text-gray-500 far fa-file-alt text-lg leading-lg mr-2" />
-                  Docs
-                </a>
-              </li>
-            </ul>
-            <ul class="flex flex-col lg:flex-row list-none lg:ml-auto">
-              <li class="flex items-center">
-                <IndexDropdown />
-              </li>
-              <li class="flex items-center">
-                <a
-                  class="hover:text-gray-600 text-gray-800 px-3 py-4 lg:py-2 flex items-center text-xs uppercase font-bold"
-                  href="#pablo"
-                >
-                  <i class="text-gray-500 fab fa-facebook text-lg leading-lg " />
-                  <span class="lg:hidden inline-block ml-2">Share</span>
-                </a>
-              </li>
-
-              <li class="flex items-center">
-                <a
-                  class="hover:text-gray-600 text-gray-800 px-3 py-4 lg:py-2 flex items-center text-xs uppercase font-bold"
-                  href="#pablo"
-                >
-                  <i class="text-gray-500 fab fa-twitter text-lg leading-lg " />
-                  <span class="lg:hidden inline-block ml-2">Tweet</span>
-                </a>
-              </li>
-
-              <li class="flex items-center">
-                <a
-                  class="hover:text-gray-600 text-gray-800 px-3 py-4 lg:py-2 flex items-center text-xs uppercase font-bold"
-                  href="#pablo"
-                >
-                  <i class="text-gray-500 fab fa-github text-lg leading-lg " />
-                  <span class="lg:hidden inline-block ml-2">Star</span>
-                </a>
-              </li>
-
-              <li class="flex items-center">
-                <button
-                  class="bg-blue-500 text-white active:bg-blue-600 text-xs font-bold uppercase px-4 py-2 rounded shadow hover:shadow-lg outline-none focus:outline-none lg:mr-1 lg:mb-0 ml-3 mb-3"
-                  type="button"
-                  style={{ transition: "all .15s ease" }}
-                >
-                  <i class="fas fa-arrow-alt-circle-down"></i> Download
-                </button>
-              </li>
-            </ul>
-          </div>
-        </div>
-      </nav>
-    </>
-  );
-}
+export default {
+  data() {
+    return {
+      navbarOpen: false,
+    };
+  },
+  methods: {
+    setNavbarOpen: function () {
+      this.navbarOpen = !this.navbarOpen;
+    },
+  },
+  components: {
+    IndexDropdown,
+  },
+};
+</script>
