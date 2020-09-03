32f4ab8ed96271ae12d5cbe044a4bfa2770deee8
EINazare <emanuelioannazare@gmail.com>
2020-09-03 11:55:27 +0300

    refactor: change react code to vuejs

--- src/components/Cards/CardStats.vue
+++ src/components/Cards/CardStats.vue
@@ -1,83 +1,83 @@
-import React from "react";
-import PropTypes from "prop-types";
-
-export default function CardStats({
-  statSubtitle,
-  statTitle,
-  statArrow,
-  statPercent,
-  statPercentColor,
-  statDescripiron,
-  statIconName,
-  statIconColor,
-}) {
-  return (
-    <>
-      <div className="relative flex flex-col min-w-0 break-words bg-white rounded mb-6 xl:mb-0 shadow-lg">
-        <div className="flex-auto p-4">
-          <div className="flex flex-wrap">
-            <div className="relative w-full pr-4 max-w-full flex-grow flex-1">
-              <h5 className="text-gray-500 uppercase font-bold text-xs">
-                {statSubtitle}
-              </h5>
-              <span className="font-semibold text-xl text-gray-800">
-                {statTitle}
-              </span>
-            </div>
-            <div className="relative w-auto pl-4 flex-initial">
-              <div
-                className={
-                  "text-white p-3 text-center inline-flex items-center justify-center w-12 h-12 shadow-lg rounded-full " +
-                  statIconColor
-                }
-              >
-                <i className={statIconName}></i>
-              </div>
-            </div>
+<template>
+  <div
+    class="relative flex flex-col min-w-0 break-words bg-white rounded mb-6 xl:mb-0 shadow-lg"
+  >
+    <div class="flex-auto p-4">
+      <div class="flex flex-wrap">
+        <div class="relative w-full pr-4 max-w-full flex-grow flex-1">
+          <h5 class="text-gray-500 uppercase font-bold text-xs">
+            {{ statSubtitle }}
+          </h5>
+          <span class="font-semibold text-xl text-gray-800">
+            {{ statTitle }}
+          </span>
+        </div>
+        <div class="relative w-auto pl-4 flex-initial">
+          <div
+            class="text-white p-3 text-center inline-flex items-center justify-center w-12 h-12 shadow-lg rounded-full"
+            :class="[statIconColor]"
+          >
+            <i :class="[statIconName]"></i>
           </div>
-          <p className="text-sm text-gray-500 mt-4">
-            <span className={statPercentColor + " mr-2"}>
-              <i
-                className={
-                  statArrow === "up"
-                    ? "fas fa-arrow-up"
-                    : statArrow === "down"
-                    ? "fas fa-arrow-down"
-                    : ""
-                }
-              ></i>{" "}
-              {statPercent}%
-            </span>
-            <span className="whitespace-no-wrap">{statDescripiron}</span>
-          </p>
         </div>
       </div>
-    </>
-  );
-}
-
-CardStats.defaultProps = {
-  statSubtitle: "Traffic",
-  statTitle: "350,897",
-  statArrow: "up",
-  statPercent: "3.48",
-  statPercentColor: "text-green-500",
-  statDescripiron: "Since last month",
-  statIconName: "far fa-chart-bar",
-  statIconColor: "bg-red-500",
+      <p class="text-sm text-gray-500 mt-4">
+        <span class="mr-2" :class="[statPercentColor]">
+          <i
+            :class="[
+              statArrow === 'up' ? `fas fa-arrow-up` : `fas fa-arrow-down`,
+            ]"
+          ></i>
+          {{ statPercent }}%
+        </span>
+        <span class="whitespace-no-wrap">{{ statDescripiron }}</span>
+      </p>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: "card-stats",
+  props: {
+    statSubtitle: {
+      type: String,
+      default: "Traffic",
+    },
+    statTitle: {
+      type: String,
+      default: "350,897",
+    },
+    statArrow: {
+      default: "up",
+      validator: function (value) {
+        // The value must match one of these strings
+        return ["up", "down"].indexOf(value) !== -1;
+      },
+    },
+    statPercent: {
+      type: String,
+      default: "3.48",
+    },
+    // can be any of the text color utilities
+    // from tailwindcss
+    statPercentColor: {
+      type: String,
+      default: "text-green-500",
+    },
+    statDescripiron: {
+      type: String,
+      default: "Since last month",
+    },
+    statIconName: {
+      type: String,
+      default: "far fa-chart-bar",
+    },
+    // can be any of the background color utilities
+    // from tailwindcss
+    statIconColor: {
+      type: String,
+      default: "bg-red-500",
+    },
+  },
 };
-
-CardStats.propTypes = {
-  statSubtitle: PropTypes.string,
-  statTitle: PropTypes.string,
-  statArrow: PropTypes.oneOf(["up", "down"]),
-  statPercent: PropTypes.string,
-  // can be any of the text color utilities
-  // from tailwindcss
-  statPercentColor: PropTypes.string,
-  statDescripiron: PropTypes.string,
-  statIconName: PropTypes.string,
-  // can be any of the background color utilities
-  // from tailwindcss
-  statIconColor: PropTypes.string,
-};
+</script>
