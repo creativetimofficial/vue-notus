8f153aae9141385644e8f0c727b7fc73496b14f8
EINazare <emanuelioannazare@gmail.com>
2020-09-03 16:06:14 +0300

    refactor: add green bg

--- src/components/Cards/CardLineChart.vue
+++ src/components/Cards/CardLineChart.vue
@@ -1,25 +1,23 @@
 <template>
-  <div class="w-full xl:w-8/12 mb-12 xl:mb-0 px-4">
-    <div
-      class="relative flex flex-col min-w-0 break-words w-full mb-6 shadow-lg rounded bg-gray-900"
-    >
-      <div class="rounded-t mb-0 px-4 py-3 bg-transparent">
-        <div class="flex flex-wrap items-center">
-          <div class="relative w-full max-w-full flex-grow flex-1">
-            <h6 class="uppercase text-gray-200 mb-1 text-xs font-semibold">
-              Overview
-            </h6>
-            <h2 class="text-white text-xl font-semibold">
-              Sales value
-            </h2>
-          </div>
+  <div
+    class="relative flex flex-col min-w-0 break-words w-full mb-6 shadow-lg rounded bg-green-900"
+  >
+    <div class="rounded-t mb-0 px-4 py-3 bg-transparent">
+      <div class="flex flex-wrap items-center">
+        <div class="relative w-full max-w-full flex-grow flex-1">
+          <h6 class="uppercase text-gray-200 mb-1 text-xs font-semibold">
+            Overview
+          </h6>
+          <h2 class="text-white text-xl font-semibold">
+            Sales value
+          </h2>
         </div>
       </div>
-      <div class="p-4 flex-auto">
-        <!-- Chart -->
-        <div class="relative" style="height: 350px;">
-          <canvas id="line-chart"></canvas>
-        </div>
+    </div>
+    <div class="p-4 flex-auto">
+      <!-- Chart -->
+      <div class="relative" style="height: 350px;">
+        <canvas id="line-chart"></canvas>
       </div>
     </div>
   </div>
