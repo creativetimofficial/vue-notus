23400da59d389bacf17da37d15ff1be18f322b0d
EINazare <emanuelioannazare@gmail.com>
2020-09-03 17:08:23 +0300

    feature: add deploysh file for deploying to gh pages

---
+++ deploy.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env sh
+
+# abort on errors
+set -e
+
+# build
+npm run build
+
+# navigate into the build output directory
+cd dist
+
+# if you are deploying to a custom domain
+echo 'https://demos.creative-tim.com/tailwind-webapp-vuejs' > CNAME
+
+git init
+git add -A
+git commit -m 'deploy'
+
+# if you are deploying to https://<USERNAME>.github.io
+# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
+
+# if you are deploying to https://<USERNAME>.github.io/<REPO>
+git push -f git@github.com:creativetimofficial/tailwind-webapp-vuejs.git feature/v1.0.0-live:gh-pages
+
+cd -
