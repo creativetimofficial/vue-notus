4d64db5be510cb8cf62a8a23050e766b8b04353d
EINazare <emanuelioannazare@gmail.com>
2020-09-03 15:18:23 +0300

    refactor: change react code to vuejs

--- src/views/auth/Register.vue
+++ src/views/auth/Register.vue
@@ -1,132 +1,129 @@
-import React from "react";
-
-export default function Register() {
-  return (
-    <>
-      <div class="container mx-auto px-4 h-full">
-        <div class="flex content-center items-center justify-center h-full">
-          <div class="w-full lg:w-6/12 px-4">
-            <div class="relative flex flex-col min-w-0 break-words w-full mb-6 shadow-lg rounded-lg bg-gray-300 border-0">
-              <div class="rounded-t mb-0 px-6 py-6">
-                <div class="text-center mb-3">
-                  <h6 class="text-gray-600 text-sm font-bold">
-                    Sign up with
-                  </h6>
-                </div>
-                <div class="btn-wrapper text-center">
-                  <button
-                    class="bg-white active:bg-gray-100 text-gray-800 font-normal px-4 py-2 rounded outline-none focus:outline-none mr-2 mb-1 uppercase shadow hover:shadow-md inline-flex items-center font-bold text-xs"
-                    type="button"
-                    style={{ transition: "all .15s ease" }}
-                  >
-                    <img
-                      alt="..."
-                      class="w-5 mr-1"
-                      src={require("assets/img/github.svg")}
-                    />
-                    Github
-                  </button>
-                  <button
-                    class="bg-white active:bg-gray-100 text-gray-800 font-normal px-4 py-2 rounded outline-none focus:outline-none mr-1 mb-1 uppercase shadow hover:shadow-md inline-flex items-center font-bold text-xs"
-                    type="button"
-                    style={{ transition: "all .15s ease" }}
-                  >
-                    <img
-                      alt="..."
-                      class="w-5 mr-1"
-                      src={require("assets/img/google.svg")}
-                    />
-                    Google
-                  </button>
-                </div>
-                <hr class="mt-6 border-b-1 border-gray-400" />
-              </div>
-              <div class="flex-auto px-4 lg:px-10 py-10 pt-0">
-                <div class="text-gray-500 text-center mb-3 font-bold">
-                  <small>Or sign up with credentials</small>
-                </div>
-                <form>
-                  <div class="relative w-full mb-3">
-                    <label
-                      class="block uppercase text-gray-700 text-xs font-bold mb-2"
-                      htmlFor="grid-password"
-                    >
-                      Name
-                    </label>
-                    <input
-                      type="email"
-                      class="px-3 py-3 placeholder-gray-400 text-gray-700 bg-white rounded text-sm shadow focus:outline-none focus:shadow-outline w-full"
-                      placeholder="Name"
-                      style={{ transition: "all .15s ease" }}
-                    />
-                  </div>
-
-                  <div class="relative w-full mb-3">
-                    <label
-                      class="block uppercase text-gray-700 text-xs font-bold mb-2"
-                      htmlFor="grid-password"
-                    >
-                      Email
-                    </label>
-                    <input
-                      type="email"
-                      class="px-3 py-3 placeholder-gray-400 text-gray-700 bg-white rounded text-sm shadow focus:outline-none focus:shadow-outline w-full"
-                      placeholder="Email"
-                      style={{ transition: "all .15s ease" }}
-                    />
-                  </div>
-
-                  <div class="relative w-full mb-3">
-                    <label
-                      class="block uppercase text-gray-700 text-xs font-bold mb-2"
-                      htmlFor="grid-password"
-                    >
-                      Password
-                    </label>
-                    <input
-                      type="password"
-                      class="px-3 py-3 placeholder-gray-400 text-gray-700 bg-white rounded text-sm shadow focus:outline-none focus:shadow-outline w-full"
-                      placeholder="Password"
-                      style={{ transition: "all .15s ease" }}
-                    />
-                  </div>
-
-                  <div>
-                    <label class="inline-flex items-center cursor-pointer">
-                      <input
-                        id="customCheckLogin"
-                        type="checkbox"
-                        class="form-checkbox text-gray-800 ml-1 w-5 h-5"
-                        style={{ transition: "all .15s ease" }}
-                      />
-                      <span class="ml-2 text-sm font-semibold text-gray-700">
-                        I agree with the
-                        <a
-                          href="#pablo"
-                          class="text-blue-500"
-                          onClick={(e) => e.preventDefault()}
-                        >
-                          Privacy Policy
-                        </a>
-                      </span>
-                    </label>
-                  </div>
-
-                  <div class="text-center mt-6">
-                    <button
-                      class="bg-gray-900 text-white active:bg-gray-700 text-sm font-bold uppercase px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 w-full"
-                      type="button"
-                      style={{ transition: "all .15s ease" }}
-                    >
-                      Create Account
-                    </button>
-                  </div>
-                </form>
-              </div>
+<template>
+  <div class="container mx-auto px-4 h-full">
+    <div class="flex content-center items-center justify-center h-full">
+      <div class="w-full lg:w-6/12 px-4">
+        <div
+          class="relative flex flex-col min-w-0 break-words w-full mb-6 shadow-lg rounded-lg bg-gray-300 border-0"
+        >
+          <div class="rounded-t mb-0 px-6 py-6">
+            <div class="text-center mb-3">
+              <h6 class="text-gray-600 text-sm font-bold">
+                Sign up with
+              </h6>
             </div>
+            <div class="btn-wrapper text-center">
+              <button
+                class="bg-white active:bg-gray-100 text-gray-800 font-normal px-4 py-2 rounded outline-none focus:outline-none mr-2 mb-1 uppercase shadow hover:shadow-md inline-flex items-center font-bold text-xs"
+                type="button"
+                style="transition: all 0.15s ease;"
+              >
+                <img alt="..." class="w-5 mr-1" :src="github" />
+                Github
+              </button>
+              <button
+                class="bg-white active:bg-gray-100 text-gray-800 font-normal px-4 py-2 rounded outline-none focus:outline-none mr-1 mb-1 uppercase shadow hover:shadow-md inline-flex items-center font-bold text-xs"
+                type="button"
+                style="transition: all 0.15s ease;"
+              >
+                <img alt="..." class="w-5 mr-1" :src="google" />
+                Google
+              </button>
+            </div>
+            <hr class="mt-6 border-b-1 border-gray-400" />
+          </div>
+          <div class="flex-auto px-4 lg:px-10 py-10 pt-0">
+            <div class="text-gray-500 text-center mb-3 font-bold">
+              <small>Or sign up with credentials</small>
+            </div>
+            <form>
+              <div class="relative w-full mb-3">
+                <label
+                  class="block uppercase text-gray-700 text-xs font-bold mb-2"
+                  htmlFor="grid-password"
+                >
+                  Name
+                </label>
+                <input
+                  type="email"
+                  class="px-3 py-3 placeholder-gray-400 text-gray-700 bg-white rounded text-sm shadow focus:outline-none focus:shadow-outline w-full"
+                  placeholder="Name"
+                  style="transition: all 0.15s ease;"
+                />
+              </div>
+
+              <div class="relative w-full mb-3">
+                <label
+                  class="block uppercase text-gray-700 text-xs font-bold mb-2"
+                  htmlFor="grid-password"
+                >
+                  Email
+                </label>
+                <input
+                  type="email"
+                  class="px-3 py-3 placeholder-gray-400 text-gray-700 bg-white rounded text-sm shadow focus:outline-none focus:shadow-outline w-full"
+                  placeholder="Email"
+                  style="transition: all 0.15s ease;"
+                />
+              </div>
+
+              <div class="relative w-full mb-3">
+                <label
+                  class="block uppercase text-gray-700 text-xs font-bold mb-2"
+                  htmlFor="grid-password"
+                >
+                  Password
+                </label>
+                <input
+                  type="password"
+                  class="px-3 py-3 placeholder-gray-400 text-gray-700 bg-white rounded text-sm shadow focus:outline-none focus:shadow-outline w-full"
+                  placeholder="Password"
+                  style="transition: all 0.15s ease;"
+                />
+              </div>
+
+              <div>
+                <label class="inline-flex items-center cursor-pointer">
+                  <input
+                    id="customCheckLogin"
+                    type="checkbox"
+                    class="form-checkbox text-gray-800 ml-1 w-5 h-5"
+                    style="transition: all 0.15s ease;"
+                  />
+                  <span class="ml-2 text-sm font-semibold text-gray-700">
+                    I agree with the
+                    <a href="javascript:void(0)" class="text-blue-500">
+                      Privacy Policy
+                    </a>
+                  </span>
+                </label>
+              </div>
+
+              <div class="text-center mt-6">
+                <button
+                  class="bg-gray-900 text-white active:bg-gray-700 text-sm font-bold uppercase px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 w-full"
+                  type="button"
+                  style="transition: all 0.15s ease;"
+                >
+                  Create Account
+                </button>
+              </div>
+            </form>
           </div>
         </div>
       </div>
-    </>
-  );
-}
+    </div>
+  </div>
+</template>
+<script>
+import github from "assets/img/github.svg";
+import google from "assets/img/google.svg";
+
+export default {
+  data() {
+    return {
+      github,
+      google,
+    };
+  },
+};
+</script>
