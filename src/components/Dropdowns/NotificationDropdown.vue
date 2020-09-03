b0f40e9457051b980ecfb40d377270b1ad00e192
EINazare <emanuelioannazare@gmail.com>
2020-09-03 12:11:15 +0300

    refactor: change react code to vuejs

--- src/components/Dropdowns/NotificationDropdown.vue
+++ src/components/Dropdowns/NotificationDropdown.vue
@@ -1,82 +1,70 @@
-import React from "react";
+<template>
+  <div>
+    <a
+      class="text-gray-600 block py-1 px-3"
+      ref="btnDropdownRef"
+      v-on:click="toggleDropdown($event)"
+    >
+      <i class="fas fa-bell"></i>
+    </a>
+    <div
+      ref="popoverDropdownRef"
+      class="bg-white text-base z-50 float-left py-2 list-none text-left rounded shadow-lg mt-1"
+      style="min-width: 12rem;"
+      v-bind:class="{
+        hidden: !dropdownPopoverShow,
+        block: dropdownPopoverShow,
+      }"
+    >
+      <a
+        href="javascript:void(0);"
+        class="text-sm py-2 px-4 font-normal block w-full whitespace-no-wrap bg-transparent text-gray-800"
+      >
+        Action
+      </a>
+      <a
+        href="javascript:void(0);"
+        class="text-sm py-2 px-4 font-normal block w-full whitespace-no-wrap bg-transparent text-gray-800"
+      >
+        Another action
+      </a>
+      <a
+        href="javascript:void(0);"
+        class="text-sm py-2 px-4 font-normal block w-full whitespace-no-wrap bg-transparent text-gray-800"
+      >
+        Something else here
+      </a>
+      <div class="h-0 my-2 border border-solid border-gray-200" />
+      <a
+        href="javascript:void(0);"
+        class="text-sm py-2 px-4 font-normal block w-full whitespace-no-wrap bg-transparent text-gray-800"
+      >
+        Seprated link
+      </a>
+    </div>
+  </div>
+</template>
+<script>
 import { createPopper } from "@popperjs/core";
 
-const NotificationDropdown = () => {
-  // dropdown props
-  const [dropdownPopoverShow, setDropdownPopoverShow] = React.useState(false);
-  const btnDropdownRef = React.createRef();
-  const popoverDropdownRef = React.createRef();
-  const openDropdownPopover = () => {
-    console.log("hey");
-    createPopper(btnDropdownRef.current, popoverDropdownRef.current, {
-      placement: "bottom-end",
-    });
-    setDropdownPopoverShow(true);
-  };
-  const closeDropdownPopover = () => {
-    setDropdownPopoverShow(false);
-  };
-  return (
-    <>
-      <a
-        class="text-gray-600 block py-1 px-3"
-        href="#pablo"
-        ref={btnDropdownRef}
-        onClick={(e) => {
-          e.preventDefault();
-          dropdownPopoverShow ? closeDropdownPopover() : openDropdownPopover();
-        }}
-      >
-        <i class="fas fa-bell"></i>
-      </a>
-      <div
-        ref={popoverDropdownRef}
-        className={
-          (dropdownPopoverShow ? "block " : "hidden ") +
-          "bg-white text-base z-50 float-left py-2 list-none text-left rounded shadow-lg mt-1"
-        }
-        style={{ minWidth: "12rem" }}
-      >
-        <a
-          href="#pablo"
-          className={
-            "text-sm py-2 px-4 font-normal block w-full whitespace-no-wrap bg-transparent text-gray-800"
-          }
-          onClick={(e) => e.preventDefault()}
-        >
-          Action
-        </a>
-        <a
-          href="#pablo"
-          className={
-            "text-sm py-2 px-4 font-normal block w-full whitespace-no-wrap bg-transparent text-gray-800"
-          }
-          onClick={(e) => e.preventDefault()}
-        >
-          Another action
-        </a>
-        <a
-          href="#pablo"
-          className={
-            "text-sm py-2 px-4 font-normal block w-full whitespace-no-wrap bg-transparent text-gray-800"
-          }
-          onClick={(e) => e.preventDefault()}
-        >
-          Something else here
-        </a>
-        <div class="h-0 my-2 border border-solid border-gray-200" />
-        <a
-          href="#pablo"
-          className={
-            "text-sm py-2 px-4 font-normal block w-full whitespace-no-wrap bg-transparent text-gray-800"
-          }
-          onClick={(e) => e.preventDefault()}
-        >
-          Seprated link
-        </a>
-      </div>
-    </>
-  );
+export default {
+  data() {
+    return {
+      dropdownPopoverShow: false,
+    };
+  },
+  methods: {
+    toggleDropdown: function (event) {
+      event.preventDefault();
+      if (this.dropdownPopoverShow) {
+        this.dropdownPopoverShow = false;
+      } else {
+        this.dropdownPopoverShow = true;
+        createPopper(this.$refs.btnDropdownRef, this.$refs.popoverDropdownRef, {
+          placement: "bottom-end",
+        });
+      }
+    },
+  },
 };
-
-export default NotificationDropdown;
+</script>
