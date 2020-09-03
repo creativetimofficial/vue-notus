0c27c713efcc72a30ce529acf9331211cb98340c
EINazare <emanuelioannazare@gmail.com>
2020-09-03 15:04:27 +0300

    refactor: change react code to vuejs

--- src/components/Footers/FooterSmall.vue
+++ src/components/Footers/FooterSmall.vue
@@ -1,68 +1,77 @@
-import React from "react";
-
-export default function FooterSmall(props) {
-  return (
-    <>
-      <footer
-        className={
-          (props.absolute
-            ? "absolute w-full bottom-0 bg-gray-900"
-            : "relative") + " pb-6"
-        }
+<template>
+  <footer
+    class="pb-6"
+    :class="[absolute ? 'absolute w-full bottom-0 bg-gray-900' : 'relative']"
+  >
+    <div class="container mx-auto px-4">
+      <hr class="mb-6 border-b-1 border-gray-700" />
+      <div
+        class="flex flex-wrap items-center md:justify-between justify-center"
       >
-        <div class="container mx-auto px-4">
-          <hr class="mb-6 border-b-1 border-gray-700" />
-          <div class="flex flex-wrap items-center md:justify-between justify-center">
-            <div class="w-full md:w-4/12 px-4">
-              <div class="text-sm text-white font-semibold py-1">
-                Copyright © {new Date().getFullYear()}
-                <a
-                  href="https://www.creative-tim.com"
-                  class="text-white hover:text-gray-400 text-sm font-semibold py-1"
-                >
-                  Creative Tim
-                </a>
-              </div>
-            </div>
-            <div class="w-full md:w-8/12 px-4">
-              <ul class="flex flex-wrap list-none md:justify-end  justify-center">
-                <li>
-                  <a
-                    href="https://www.creative-tim.com"
-                    class="text-white hover:text-gray-400 text-sm font-semibold block py-1 px-3"
-                  >
-                    Creative Tim
-                  </a>
-                </li>
-                <li>
-                  <a
-                    href="https://www.creative-tim.com/presentation"
-                    class="text-white hover:text-gray-400 text-sm font-semibold block py-1 px-3"
-                  >
-                    About Us
-                  </a>
-                </li>
-                <li>
-                  <a
-                    href="http://blog.creative-tim.com"
-                    class="text-white hover:text-gray-400 text-sm font-semibold block py-1 px-3"
-                  >
-                    Blog
-                  </a>
-                </li>
-                <li>
-                  <a
-                    href="https://github.com/creativetimofficial/argon-design-system/blob/master/LICENSE.md"
-                    class="text-white hover:text-gray-400 text-sm font-semibold block py-1 px-3"
-                  >
-                    MIT License
-                  </a>
-                </li>
-              </ul>
-            </div>
+        <div class="w-full md:w-4/12 px-4">
+          <div class="text-sm text-white font-semibold py-1">
+            Copyright © {{ date }}
+            <a
+              href="https://www.creative-tim.com"
+              class="text-white hover:text-gray-400 text-sm font-semibold py-1"
+            >
+              Creative Tim
+            </a>
           </div>
         </div>
-      </footer>
-    </>
-  );
+        <div class="w-full md:w-8/12 px-4">
+          <ul class="flex flex-wrap list-none md:justify-end justify-center">
+            <li>
+              <a
+                href="https://www.creative-tim.com"
+                class="text-white hover:text-gray-400 text-sm font-semibold block py-1 px-3"
+              >
+                Creative Tim
+              </a>
+            </li>
+            <li>
+              <a
+                href="https://www.creative-tim.com/presentation"
+                class="text-white hover:text-gray-400 text-sm font-semibold block py-1 px-3"
+              >
+                About Us
+              </a>
+            </li>
+            <li>
+              <a
+                href="http://blog.creative-tim.com"
+                class="text-white hover:text-gray-400 text-sm font-semibold block py-1 px-3"
+              >
+                Blog
+              </a>
+            </li>
+            <li>
+              <a
+                href="https://github.com/creativetimofficial/argon-design-system/blob/master/LICENSE.md"
+                class="text-white hover:text-gray-400 text-sm font-semibold block py-1 px-3"
+              >
+                MIT License
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </footer>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      date: new Date().getFullYear()
+    }
+  }
+  props: {
+    absolute: {
+      type: Boolean,
+      default: false
+    }
+  }
 }
+</script>
