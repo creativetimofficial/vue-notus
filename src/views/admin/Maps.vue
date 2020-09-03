8bf30af4abd36e56941ca52da4b50f6295d7a4a8
EINazare <emanuelioannazare@gmail.com>
2020-09-03 13:56:24 +0300

    refactor: change react code to vuejs

--- src/views/admin/Maps.vue
+++ src/views/admin/Maps.vue
@@ -1,31 +1,20 @@
-import React from "react";
-
-// components
-
+<template>
+  <div class="flex flex-wrap">
+    <div class="w-full px-4">
+      <div
+        class="relative flex flex-col min-w-0 break-words bg-white w-full mb-6 shadow-lg rounded"
+      >
+        <map-example />
+      </div>
+    </div>
+  </div>
+</template>
+<script>
 import MapExample from "@/components/Maps/MapExample.vue";
 
-export default function Maps() {
-  return (
-    <>
-      <div class="flex flex-wrap">
-        <div class="w-full px-4">
-          <div class="relative flex flex-col min-w-0 break-words bg-white w-full mb-6 shadow-lg rounded">
-            <MapExample
-              googleMapURL="https://maps.googleapis.com/maps/api/js?key=YOUR_KEY_HERE"
-              loadingElement={<div style={{ height: "100%" }} />}
-              containerElement={
-                <div
-                  style={{ height: "600px" }}
-                  class="relative w-full rounded"
-                />
-              }
-              mapElement={
-                <div class="rounded" style={{ height: "100%" }} />
-              }
-            />
-          </div>
-        </div>
-      </div>
-    </>
-  );
-}
+export default {
+  components: {
+    MapExample,
+  },
+};
+</script>
