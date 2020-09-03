fccec170036d1657a82562fb1e8538a2e496145e
EINazare <emanuelioannazare@gmail.com>
2020-09-03 12:08:06 +0300

    refactor: add all routes

--- src/main.js
+++ src/main.js
@@ -13,12 +13,25 @@ import App from "@/App.vue";
 // layouts
 
 import Admin from "@/layouts/Admin.vue";
+import Auth from "@/layouts/Auth.vue";
 
 // views for Admin layout
 
 import Dashboard from "@/views/admin/Dashboard.vue";
+import Settings from "@/views/admin/Settings.vue";
+import Tables from "@/views/admin/Tables.vue";
+import Maps from "@/views/admin/Maps.vue";
+
+// views for Auth layout
+
+import Login from "@/views/auth/Login.vue";
+import Register from "@/views/auth/Register.vue";
 
 // views without layouts
+
+import Landing from "@/views/Landing.vue";
+import Profile from "@/views/Profile.vue";
+import Index from "@/views/Index.vue";
 
 // routes
 
@@ -30,10 +43,48 @@ const routes = [
     children: [
       {
         path: "/admin/dashboard",
-        name: "dashboard",
         component: Dashboard,
       },
+      {
+        path: "/admin/settings",
+        component: Settings,
+      },
+      {
+        path: "/admin/tables",
+        component: Tables,
+      },
+      {
+        path: "/admin/maps",
+        component: Maps,
+      },
     ],
+  },
+  {
+    path: "/auth",
+    redirect: "/auth/login",
+    component: Auth,
+    children: [
+      {
+        path: "/auth/login",
+        component: Login,
+      },
+      {
+        path: "/auth/register",
+        component: Register,
+      },
+    ],
+  },
+  {
+    path: "/landing",
+    component: Landing,
+  },
+  {
+    path: "/profile",
+    component: Profile,
+  },
+  {
+    path: "/",
+    component: Index,
   },
   { path: "*", redirect: "/" },
 ];
