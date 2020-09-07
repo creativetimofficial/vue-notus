e75c71ea11aa389f53688df72ef21ca9cc6db746
EINazare <emanuelioannazare@gmail.com>
2020-09-07 18:17:39 +0300

    fix: solve responsive issues

--- src/components/Footers/FooterAdmin.vue
+++ src/components/Footers/FooterAdmin.vue
@@ -6,7 +6,7 @@
         class="flex flex-wrap items-center md:justify-between justify-center"
       >
         <div class="w-full md:w-4/12 px-4">
-          <div class="text-sm text-gray-600 font-semibold py-1">
+          <div class="text-sm text-gray-600 font-semibold py-1 text-center md:text-left">
             Copyright © {{ date }}
             <a
               href="https://www.creative-tim.com"
