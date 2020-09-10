62d0664530fc801da61556a1c841c9b8dd039065
EINazare <emanuelioannazare@gmail.com>
2020-09-10 19:17:20 +0300

    fix: solve broken link

--- deploy.sh
+++ deploy.sh
@@ -25,7 +25,7 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f https://github.com/creativetimofficial/tailwind-webapp-vuejs.git master:gh-pages
+git push -f https://github.com/creativetimofficial/vue-tailwind-webapp.git master:gh-pages
 
 
 cd -
