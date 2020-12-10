5372cefec3960b046fd8828ace87e91109aed341
EINazare <emanuelioannazare@gmail.com>
2020-12-10 09:46:24 +0200

    fix: solve download issues

--- src/components/Navbars/IndexNavbar.vue
+++ src/components/Navbars/IndexNavbar.vue
@@ -80,12 +80,13 @@
           </li>
 
           <li class="flex items-center">
-            <button
-              class="bg-green-500 text-white active:bg-green-600 text-xs font-bold uppercase px-4 py-2 rounded shadow hover:shadow-lg outline-none focus:outline-none lg:mr-1 lg:mb-0 ml-3 mb-3 ease-linear transition-all duration-150"
-              type="button"
+            <a
+              class="inline-block text-white bg-pink-500 active:bg-pink-600 text-xs font-bold uppercase px-4 py-2 rounded shadow hover:shadow-md outline-none focus:outline-none lg:mr-1 lg:mb-0 ml-3 mb-3 ease-linear transition-all duration-150"
+              href="https://www.creative-tim.com/product/vue-notus?ref=vn-index"
+              target="_blank"
             >
               <i class="fas fa-arrow-alt-circle-down"></i> Download
-            </button>
+            </a>
           </li>
         </ul>
       </div>
