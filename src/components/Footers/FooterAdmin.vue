d11b0f277a5fa544a4067765c51b44a9ba6eefdd
EINazare <emanuelioannazare@gmail.com>
2021-03-18 14:43:34 +0200

    refactor: update everything

--- src/components/Footers/FooterAdmin.vue
+++ src/components/Footers/FooterAdmin.vue
@@ -1,18 +1,18 @@
 <template>
   <footer class="block py-4">
     <div class="container mx-auto px-4">
-      <hr class="mb-4 border-b-1 border-gray-300" />
+      <hr class="mb-4 border-b-1 border-coolGray-200" />
       <div
         class="flex flex-wrap items-center md:justify-between justify-center"
       >
         <div class="w-full md:w-4/12 px-4">
           <div
-            class="text-sm text-gray-600 font-semibold py-1 text-center md:text-left"
+            class="text-sm text-coolGray-500 font-semibold py-1 text-center md:text-left"
           >
             Copyright © {{ date }}
             <a
               href="https://www.creative-tim.com?ref=vn-footer-admin"
-              class="text-gray-600 hover:text-gray-800 text-sm font-semibold py-1"
+              class="text-coolGray-500 hover:text-coolGray-700 text-sm font-semibold py-1"
             >
               Creative Tim
             </a>
@@ -23,7 +23,7 @@
             <li>
               <a
                 href="https://www.creative-tim.com?ref=vn-footer-admin"
-                class="text-gray-700 hover:text-gray-900 text-sm font-semibold block py-1 px-3"
+                class="text-coolGray-600 hover:text-coolGray-800 text-sm font-semibold block py-1 px-3"
               >
                 Creative Tim
               </a>
@@ -31,7 +31,7 @@
             <li>
               <a
                 href="https://www.creative-tim.com/presentation?ref=vn-footer-admin"
-                class="text-gray-700 hover:text-gray-900 text-sm font-semibold block py-1 px-3"
+                class="text-coolGray-600 hover:text-coolGray-800 text-sm font-semibold block py-1 px-3"
               >
                 About Us
               </a>
@@ -39,7 +39,7 @@
             <li>
               <a
                 href="http://blog.creative-tim.com?ref=vn-footer-admin"
-                class="text-gray-700 hover:text-gray-900 text-sm font-semibold block py-1 px-3"
+                class="text-coolGray-600 hover:text-coolGray-800 text-sm font-semibold block py-1 px-3"
               >
                 Blog
               </a>
@@ -47,7 +47,7 @@
             <li>
               <a
                 href="https://github.com/creativetimofficial/vue-notus/blob/main/LICENSE.md?ref=vn-footer-admin"
-                class="text-gray-700 hover:text-gray-900 text-sm font-semibold block py-1 px-3"
+                class="text-coolGray-600 hover:text-coolGray-800 text-sm font-semibold block py-1 px-3"
               >
                 MIT License
               </a>
