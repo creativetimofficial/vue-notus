e3a339503ed89a92d8b567baca1d2221b1ab471a
EINazare <emanuelioannazare@gmail.com>
2021-03-18 14:43:43 +0200

    refactor: update everything

--- src/layouts/Admin.vue
+++ src/layouts/Admin.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <sidebar />
-    <div class="relative md:ml-64 bg-gray-200">
+    <div class="relative md:ml-64 bg-coolGray-100">
       <admin-navbar />
       <header-stats />
       <div class="px-4 md:px-10 mx-auto w-full -m-24">
