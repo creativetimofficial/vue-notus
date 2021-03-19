470965bacb870e2dc224c4e810f0555ae5fe4972
EINazare <emanuelioannazare@gmail.com>
2021-03-19 11:46:49 +0200

    refactor: change coolGray with blueGray

--- src/components/Navbars/AuthNavbar.vue
+++ src/components/Navbars/AuthNavbar.vue
@@ -30,11 +30,11 @@
         <ul class="flex flex-col lg:flex-row list-none mr-auto">
           <li class="flex items-center">
             <a
-              class="lg:text-white lg:hover:text-coolGray-200 text-coolGray-700 px-3 py-4 lg:py-2 flex items-center text-xs uppercase font-bold"
+              class="lg:text-white lg:hover:text-blueGray-200 text-blueGray-700 px-3 py-4 lg:py-2 flex items-center text-xs uppercase font-bold"
               href="https://www.creative-tim.com/learning-lab/tailwind/vue/overview/notus?ref=vn-auth-navbar"
             >
               <i
-                class="lg:text-coolGray-200 text-coolGray-400 far fa-file-alt text-lg leading-lg mr-2"
+                class="lg:text-blueGray-200 text-blueGray-400 far fa-file-alt text-lg leading-lg mr-2"
               />
               Docs
             </a>
@@ -46,12 +46,12 @@
           </li>
           <li class="flex items-center">
             <a
-              class="lg:text-white lg:hover:text-coolGray-200 text-coolGray-700 px-3 py-4 lg:py-2 flex items-center text-xs uppercase font-bold"
+              class="lg:text-white lg:hover:text-blueGray-200 text-blueGray-700 px-3 py-4 lg:py-2 flex items-center text-xs uppercase font-bold"
               href="https://www.facebook.com/sharer/sharer.php?u=https%3A%2F%2Fdemos.creative-tim.com%2Fvue-notus%2F%23%2F"
               target="_blank"
             >
               <i
-                class="lg:text-coolGray-200 text-coolGray-400 fab fa-facebook text-lg leading-lg"
+                class="lg:text-blueGray-200 text-blueGray-400 fab fa-facebook text-lg leading-lg"
               />
               <span class="lg:hidden inline-block ml-2">Share</span>
             </a>
@@ -59,12 +59,12 @@
 
           <li class="flex items-center">
             <a
-              class="lg:text-white lg:hover:text-coolGray-200 text-coolGray-700 px-3 py-4 lg:py-2 flex items-center text-xs uppercase font-bold"
+              class="lg:text-white lg:hover:text-blueGray-200 text-blueGray-700 px-3 py-4 lg:py-2 flex items-center text-xs uppercase font-bold"
               href="https://twitter.com/intent/tweet?url=https%3A%2F%2Fdemos.creative-tim.com%2Fvue-notus%2F%23%2F&text=Start%20your%20development%20with%20a%20Free%20Tailwind%20CSS%20and%20VueJS%20UI%20Kit%20and%20Admin.%20Let%20Vue%20Notus%20amaze%20you%20with%20its%20cool%20features%20and%20build%20tools%20and%20get%20your%20project%20to%20a%20whole%20new%20level.%20"
               target="_blank"
             >
               <i
-                class="lg:text-coolGray-200 text-coolGray-400 fab fa-twitter text-lg leading-lg"
+                class="lg:text-blueGray-200 text-blueGray-400 fab fa-twitter text-lg leading-lg"
               />
               <span class="lg:hidden inline-block ml-2">Tweet</span>
             </a>
@@ -72,12 +72,12 @@
 
           <li class="flex items-center">
             <a
-              class="lg:text-white lg:hover:text-coolGray-200 text-coolGray-700 px-3 py-4 lg:py-2 flex items-center text-xs uppercase font-bold"
+              class="lg:text-white lg:hover:text-blueGray-200 text-blueGray-700 px-3 py-4 lg:py-2 flex items-center text-xs uppercase font-bold"
               href="https://github.com/creativetimofficial/vue-notus?ref=vn-auth-navbar"
               target="_blank"
             >
               <i
-                class="lg:text-coolGray-200 text-coolGray-400 fab fa-github text-lg leading-lg"
+                class="lg:text-blueGray-200 text-blueGray-400 fab fa-github text-lg leading-lg"
               />
               <span class="lg:hidden inline-block ml-2">Star</span>
             </a>
@@ -85,7 +85,7 @@
 
           <li class="flex items-center">
             <button
-              class="bg-white text-coolGray-700 active:bg-coolGray-50 text-xs font-bold uppercase px-4 py-2 rounded shadow hover:shadow-md outline-none focus:outline-none lg:mr-1 lg:mb-0 ml-3 mb-3 ease-linear transition-all duration-150"
+              class="bg-white text-blueGray-700 active:bg-blueGray-50 text-xs font-bold uppercase px-4 py-2 rounded shadow hover:shadow-md outline-none focus:outline-none lg:mr-1 lg:mb-0 ml-3 mb-3 ease-linear transition-all duration-150"
               type="button"
             >
               <i class="fas fa-arrow-alt-circle-down"></i> Download
