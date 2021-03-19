ba5305f92b42dca70f10e61747962636b78738e7
EINazare <emanuelioannazare@gmail.com>
2021-03-19 11:46:46 +0200

    refactor: change coolGray with blueGray

--- src/components/Footers/FooterSmall.vue
+++ src/components/Footers/FooterSmall.vue
@@ -2,22 +2,22 @@
   <footer
     class="pb-6"
     :class="[
-      absolute ? 'absolute w-full bottom-0 bg-coolGray-800' : 'relative',
+      absolute ? 'absolute w-full bottom-0 bg-blueGray-800' : 'relative',
     ]"
   >
     <div class="container mx-auto px-4">
-      <hr class="mb-6 border-b-1 border-coolGray-600" />
+      <hr class="mb-6 border-b-1 border-blueGray-600" />
       <div
         class="flex flex-wrap items-center md:justify-between justify-center"
       >
         <div class="w-full md:w-4/12 px-4">
           <div
-            class="text-sm text-coolGray-500 font-semibold py-1 text-center md:text-left"
+            class="text-sm text-blueGray-500 font-semibold py-1 text-center md:text-left"
           >
             Copyright © {{ date }}
             <a
               href="https://www.creative-tim.com?ref=vn-footer-small"
-              class="text-white hover:text-coolGray-300 text-sm font-semibold py-1"
+              class="text-white hover:text-blueGray-300 text-sm font-semibold py-1"
             >
               Creative Tim
             </a>
@@ -28,7 +28,7 @@
             <li>
               <a
                 href="https://www.creative-tim.com?ref=vn-footer-small"
-                class="text-white hover:text-coolGray-300 text-sm font-semibold block py-1 px-3"
+                class="text-white hover:text-blueGray-300 text-sm font-semibold block py-1 px-3"
               >
                 Creative Tim
               </a>
@@ -36,7 +36,7 @@
             <li>
               <a
                 href="https://www.creative-tim.com/presentation?ref=vn-footer-small"
-                class="text-white hover:text-coolGray-300 text-sm font-semibold block py-1 px-3"
+                class="text-white hover:text-blueGray-300 text-sm font-semibold block py-1 px-3"
               >
                 About Us
               </a>
@@ -44,7 +44,7 @@
             <li>
               <a
                 href="http://blog.creative-tim.com?ref=vn-footer-small"
-                class="text-white hover:text-coolGray-300 text-sm font-semibold block py-1 px-3"
+                class="text-white hover:text-blueGray-300 text-sm font-semibold block py-1 px-3"
               >
                 Blog
               </a>
@@ -52,7 +52,7 @@
             <li>
               <a
                 href="https://github.com/creativetimofficial/vue-notus/blob/main/LICENSE.md?ref=vn-footer-small"
-                class="text-white hover:text-coolGray-300 text-sm font-semibold block py-1 px-3"
+                class="text-white hover:text-blueGray-300 text-sm font-semibold block py-1 px-3"
               >
                 MIT License
               </a>
