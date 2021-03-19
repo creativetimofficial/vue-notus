a8ae3897a5d02c79f6f4f85426615e3f92c9f042
EINazare <emanuelioannazare@gmail.com>
2021-03-19 11:46:52 +0200

    refactor: change coolGray with blueGray

--- src/components/Sidebar/Sidebar.vue
+++ src/components/Sidebar/Sidebar.vue
@@ -15,7 +15,7 @@
       </button>
       <!-- Brand -->
       <router-link
-        class="md:block text-left md:pb-2 text-coolGray-600 mr-0 inline-block whitespace-nowrap text-sm uppercase font-bold p-4 px-0"
+        class="md:block text-left md:pb-2 text-blueGray-600 mr-0 inline-block whitespace-nowrap text-sm uppercase font-bold p-4 px-0"
         to="/"
       >
         Vue Notus
@@ -36,12 +36,12 @@
       >
         <!-- Collapse header -->
         <div
-          class="md:min-w-full md:hidden block pb-4 mb-4 border-b border-solid border-coolGray-200"
+          class="md:min-w-full md:hidden block pb-4 mb-4 border-b border-solid border-blueGray-200"
         >
           <div class="flex flex-wrap">
             <div class="w-6/12">
               <router-link
-                class="md:block text-left md:pb-2 text-coolGray-600 mr-0 inline-block whitespace-nowrap text-sm uppercase font-bold p-4 px-0"
+                class="md:block text-left md:pb-2 text-blueGray-600 mr-0 inline-block whitespace-nowrap text-sm uppercase font-bold p-4 px-0"
                 to="/"
               >
                 Vue Notus
@@ -64,7 +64,7 @@
             <input
               type="text"
               placeholder="Search"
-              class="border-0 px-3 py-2 h-12 border border-solid border-coolGray-500 placeholder-coolGray-300 text-coolGray-600 bg-white rounded text-base leading-snug shadow-none outline-none focus:outline-none w-full font-normal"
+              class="border-0 px-3 py-2 h-12 border border-solid border-blueGray-500 placeholder-blueGray-300 text-blueGray-600 bg-white rounded text-base leading-snug shadow-none outline-none focus:outline-none w-full font-normal"
             />
           </div>
         </form>
@@ -73,7 +73,7 @@
         <hr class="my-4 md:min-w-full" />
         <!-- Heading -->
         <h6
-          class="md:min-w-full text-coolGray-500 text-xs uppercase font-bold block pt-1 pb-4 no-underline"
+          class="md:min-w-full text-blueGray-500 text-xs uppercase font-bold block pt-1 pb-4 no-underline"
         >
           Admin Layout Pages
         </h6>
@@ -92,12 +92,12 @@
                 :class="[
                   isActive
                     ? 'text-emerald-500 hover:text-emerald-600'
-                    : 'text-coolGray-700 hover:text-coolGray-500',
+                    : 'text-blueGray-700 hover:text-blueGray-500',
                 ]"
               >
                 <i
                   class="fas fa-tv mr-2 text-sm"
-                  :class="[isActive ? 'opacity-75' : 'text-coolGray-300']"
+                  :class="[isActive ? 'opacity-75' : 'text-blueGray-300']"
                 ></i>
                 Dashboard
               </a>
@@ -116,12 +116,12 @@
                 :class="[
                   isActive
                     ? 'text-emerald-500 hover:text-emerald-600'
-                    : 'text-coolGray-700 hover:text-coolGray-500',
+                    : 'text-blueGray-700 hover:text-blueGray-500',
                 ]"
               >
                 <i
                   class="fas fa-tools mr-2 text-sm"
-                  :class="[isActive ? 'opacity-75' : 'text-coolGray-300']"
+                  :class="[isActive ? 'opacity-75' : 'text-blueGray-300']"
                 ></i>
                 Settings
               </a>
@@ -140,12 +140,12 @@
                 :class="[
                   isActive
                     ? 'text-emerald-500 hover:text-emerald-600'
-                    : 'text-coolGray-700 hover:text-coolGray-500',
+                    : 'text-blueGray-700 hover:text-blueGray-500',
                 ]"
               >
                 <i
                   class="fas fa-table mr-2 text-sm"
-                  :class="[isActive ? 'opacity-75' : 'text-coolGray-300']"
+                  :class="[isActive ? 'opacity-75' : 'text-blueGray-300']"
                 ></i>
                 Tables
               </a>
@@ -161,12 +161,12 @@
                 :class="[
                   isActive
                     ? 'text-emerald-500 hover:text-emerald-600'
-                    : 'text-coolGray-700 hover:text-coolGray-500',
+                    : 'text-blueGray-700 hover:text-blueGray-500',
                 ]"
               >
                 <i
                   class="fas fa-map-marked mr-2 text-sm"
-                  :class="[isActive ? 'opacity-75' : 'text-coolGray-300']"
+                  :class="[isActive ? 'opacity-75' : 'text-blueGray-300']"
                 ></i>
                 Maps
               </a>
@@ -178,7 +178,7 @@
         <hr class="my-4 md:min-w-full" />
         <!-- Heading -->
         <h6
-          class="md:min-w-full text-coolGray-500 text-xs uppercase font-bold block pt-1 pb-4 no-underline"
+          class="md:min-w-full text-blueGray-500 text-xs uppercase font-bold block pt-1 pb-4 no-underline"
         >
           Auth Layout Pages
         </h6>
@@ -187,21 +187,21 @@
         <ul class="md:flex-col md:min-w-full flex flex-col list-none md:mb-4">
           <li class="items-center">
             <router-link
-              class="text-coolGray-700 hover:text-coolGray-500 text-xs uppercase py-3 font-bold block"
+              class="text-blueGray-700 hover:text-blueGray-500 text-xs uppercase py-3 font-bold block"
               to="/auth/login"
             >
-              <i class="fas fa-fingerprint text-coolGray-300 mr-2 text-sm"></i>
+              <i class="fas fa-fingerprint text-blueGray-300 mr-2 text-sm"></i>
               Login
             </router-link>
           </li>
 
           <li class="items-center">
             <router-link
-              class="text-coolGray-700 hover:text-coolGray-500 text-xs uppercase py-3 font-bold block"
+              class="text-blueGray-700 hover:text-blueGray-500 text-xs uppercase py-3 font-bold block"
               to="/auth/register"
             >
               <i
-                class="fas fa-clipboard-list text-coolGray-300 mr-2 text-sm"
+                class="fas fa-clipboard-list text-blueGray-300 mr-2 text-sm"
               ></i>
               Register
             </router-link>
@@ -212,7 +212,7 @@
         <hr class="my-4 md:min-w-full" />
         <!-- Heading -->
         <h6
-          class="md:min-w-full text-coolGray-500 text-xs uppercase font-bold block pt-1 pb-4 no-underline"
+          class="md:min-w-full text-blueGray-500 text-xs uppercase font-bold block pt-1 pb-4 no-underline"
         >
           No Layout Pages
         </h6>
@@ -221,20 +221,20 @@
         <ul class="md:flex-col md:min-w-full flex flex-col list-none md:mb-4">
           <li class="items-center">
             <router-link
-              class="text-coolGray-700 hover:text-coolGray-500 text-xs uppercase py-3 font-bold block"
+              class="text-blueGray-700 hover:text-blueGray-500 text-xs uppercase py-3 font-bold block"
               to="/landing"
             >
-              <i class="fas fa-newspaper text-coolGray-300 mr-2 text-sm"></i>
+              <i class="fas fa-newspaper text-blueGray-300 mr-2 text-sm"></i>
               Landing Page
             </router-link>
           </li>
 
           <li class="items-center">
             <router-link
-              class="text-coolGray-700 hover:text-coolGray-500 text-xs uppercase py-3 font-bold block"
+              class="text-blueGray-700 hover:text-blueGray-500 text-xs uppercase py-3 font-bold block"
               to="/profile"
             >
-              <i class="fas fa-user-circle text-coolGray-300 mr-2 text-sm"></i>
+              <i class="fas fa-user-circle text-blueGray-300 mr-2 text-sm"></i>
               Profile Page
             </router-link>
           </li>
@@ -244,7 +244,7 @@
         <hr class="my-4 md:min-w-full" />
         <!-- Heading -->
         <h6
-          class="md:min-w-full text-coolGray-500 text-xs uppercase font-bold block pt-1 pb-4 no-underline"
+          class="md:min-w-full text-blueGray-500 text-xs uppercase font-bold block pt-1 pb-4 no-underline"
         >
           Documentation
         </h6>
@@ -254,10 +254,10 @@
             <a
               href="https://www.creative-tim.com/learning-lab/tailwind/vue/colors/notus"
               target="_blank"
-              class="text-coolGray-700 hover:text-coolGray-500 text-sm block mb-4 no-underline font-semibold"
+              class="text-blueGray-700 hover:text-blueGray-500 text-sm block mb-4 no-underline font-semibold"
             >
               <i
-                class="fas fa-paint-brush mr-2 text-coolGray-300 text-base"
+                class="fas fa-paint-brush mr-2 text-blueGray-300 text-base"
               ></i>
               Styles
             </a>
@@ -267,9 +267,9 @@
             <a
               href="https://www.creative-tim.com/learning-lab/tailwind/vue/alerts/notus"
               target="_blank"
-              class="text-coolGray-700 hover:text-coolGray-500 text-sm block mb-4 no-underline font-semibold"
+              class="text-blueGray-700 hover:text-blueGray-500 text-sm block mb-4 no-underline font-semibold"
             >
-              <i class="fab fa-css3-alt mr-2 text-coolGray-300 text-base"></i>
+              <i class="fab fa-css3-alt mr-2 text-blueGray-300 text-base"></i>
               CSS Components
             </a>
           </li>
@@ -278,9 +278,9 @@
             <a
               href="https://www.creative-tim.com/learning-lab/tailwind/angular/overview/notus"
               target="_blank"
-              class="text-coolGray-700 hover:text-coolGray-500 text-sm block mb-4 no-underline font-semibold"
+              class="text-blueGray-700 hover:text-blueGray-500 text-sm block mb-4 no-underline font-semibold"
             >
-              <i class="fab fa-angular mr-2 text-coolGray-300 text-base"></i>
+              <i class="fab fa-angular mr-2 text-blueGray-300 text-base"></i>
               Angular
             </a>
           </li>
@@ -289,9 +289,9 @@
             <a
               href="https://www.creative-tim.com/learning-lab/tailwind/js/overview/notus"
               target="_blank"
-              class="text-coolGray-700 hover:text-coolGray-500 text-sm block mb-4 no-underline font-semibold"
+              class="text-blueGray-700 hover:text-blueGray-500 text-sm block mb-4 no-underline font-semibold"
             >
-              <i class="fab fa-js-square mr-2 text-coolGray-300 text-base"></i>
+              <i class="fab fa-js-square mr-2 text-blueGray-300 text-base"></i>
               Javascript
             </a>
           </li>
@@ -300,9 +300,9 @@
             <a
               href="https://www.creative-tim.com/learning-lab/tailwind/nextjs/overview/notus"
               target="_blank"
-              class="text-coolGray-700 hover:text-coolGray-500 text-sm block mb-4 no-underline font-semibold"
+              class="text-blueGray-700 hover:text-blueGray-500 text-sm block mb-4 no-underline font-semibold"
             >
-              <i class="fab fa-react mr-2 text-coolGray-300 text-base"></i>
+              <i class="fab fa-react mr-2 text-blueGray-300 text-base"></i>
               NextJS
             </a>
           </li>
@@ -311,9 +311,9 @@
             <a
               href="https://www.creative-tim.com/learning-lab/tailwind/react/overview/notus"
               target="_blank"
-              class="text-coolGray-700 hover:text-coolGray-500 text-sm block mb-4 no-underline font-semibold"
+              class="text-blueGray-700 hover:text-blueGray-500 text-sm block mb-4 no-underline font-semibold"
             >
-              <i class="fab fa-react mr-2 text-coolGray-300 text-base"></i>
+              <i class="fab fa-react mr-2 text-blueGray-300 text-base"></i>
               React
             </a>
           </li>
@@ -322,9 +322,9 @@
             <a
               href="https://www.creative-tim.com/learning-lab/tailwind/svelte/overview/notus"
               target="_blank"
-              class="text-coolGray-700 hover:text-coolGray-500 text-sm block mb-4 no-underline font-semibold"
+              class="text-blueGray-700 hover:text-blueGray-500 text-sm block mb-4 no-underline font-semibold"
             >
-              <i class="fas fa-link mr-2 text-coolGray-300 text-base"></i>
+              <i class="fas fa-link mr-2 text-blueGray-300 text-base"></i>
               Svelte
             </a>
           </li>
@@ -333,9 +333,9 @@
             <a
               href="https://www.creative-tim.com/learning-lab/tailwind/vue/overview/notus"
               target="_blank"
-              class="text-coolGray-700 hover:text-coolGray-500 text-sm block mb-4 no-underline font-semibold"
+              class="text-blueGray-700 hover:text-blueGray-500 text-sm block mb-4 no-underline font-semibold"
             >
-              <i class="fab fa-vuejs mr-2 text-coolGray-300 text-base"></i>
+              <i class="fab fa-vuejs mr-2 text-blueGray-300 text-base"></i>
               VueJS
             </a>
           </li>
