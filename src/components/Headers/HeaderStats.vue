0b48ca9e0b73156c4a3474f244e0d648d31bb630
Marco De Felice <marco.defelice@consulenti.eolo.it>
2023-05-04 16:53:32 +0200

    refactoring api fe

--- src/components/Headers/HeaderStats.vue
+++ src/components/Headers/HeaderStats.vue
@@ -49,27 +49,6 @@
               </a>
             </router-link>
           </div>
-
-          <div class="min-w px-2">
-            <router-link to="/admin/graph/budget" v-slot="{ href, navigate }">
-              <a :href="href" @click="navigate">
-                <card-stats statSubtitle="MY BUDGET" :statTitle="budget.statTitle + ' €'" :statArrow="budget.statArrow"
-                  :statPercent="budget.statPercent" :statPercentColor="budget.statPercentColor" statDescripiron=""
-                  statIconName="fas fa-box-open" statIconColor="bg-teal-500" />
-              </a>
-            </router-link>
-          </div>
-
-          <div class="min-w px-2">
-            <router-link to="/admin/graph/saving_month" v-slot="{ href, navigate }">
-              <a :href="href" @click="navigate">
-                <card-stats statSubtitle="SAVING MONTH" :statTitle="savings.statTitle + ' €'"
-                  :statArrow="savings.statArrow" :statPercent="savings.statPercent"
-                  :statPercentColor="savings.statPercentColor" statDescripiron="" statIconName="fas fa-box"
-                  statIconColor="bg-lightBlue-400" />
-              </a>
-            </router-link>
-          </div>
         </div>
       </div>
     </div>
@@ -82,7 +61,6 @@ import CardWallet from "@/components/Cards/CardWallet.vue";
 import axios from 'axios'
 // const X_API_KEY = { "X-API-KEY": "7221" };
 const DOMAIN = process.env.VUE_APP_API_PATH;
-const SAVING_PERCENTAGE = process.env.VUE_APP_BUDGET_SAVING_PERCENTAGE;
 
 export default {
   components: {
@@ -115,18 +93,6 @@ export default {
         statPercent: 0,
         statPercentColor: "text-emerald-500"
       },
-      budget: {
-        statTitle: 0,
-        statArrow: "up",
-        statPercent: 0,
-        statPercentColor: "text-emerald-500"
-      },
-      savings: {
-        statTitle: 0,
-        statArrow: "up",
-        statPercent: 0,
-        statPercentColor: "text-emerald-500"
-      },
       planned: 0
     }
   },
@@ -142,52 +108,43 @@ export default {
   },
   methods: {
     update() {
-      this.getPlanned()
       this.getMonthIncoming()
       this.getMonthexpenses()
-      this.getWallet();
+      this.getWallet()
       this.getWallets()
-    },
-    getPlanned() {
-      axios.get(DOMAIN + "/api/stats/month-wallet/planned").then((resp) => {
+      this.getWalletPlanned()
 
-        let expenses = resp.data.data.expenses * -1
-        let total = expenses - resp.data.data.incoming
-        this.planned = total
-        this.getMonthBudget()
-        this.getWalletPlanned();
-
-      }).catch((error) => {
-        console.error(error);
-      })
     },
     getWallet() {
-      axios.get(DOMAIN + "/api/stats/wallet").then((resp) => {
+      axios.get(DOMAIN + "/api/stats/total").then((resp) => {
         let data = resp.data.data
         this.wallet.statTitle = data.total.toFixed(2)
         this.getMonthBudget()
-        this.getWalletPlanned();
 
       }).catch((error) => {
         console.error(error);
       })
     },
+    
     getWalletPlanned() {
+      axios.get(DOMAIN + "/api/stats/total/planned").then((resp) => {
 
-        let totalWallet = parseFloat(this.wallet.statTitle)
-        let total = totalWallet - this.planned
-
-        this.walletPlanned.statTitle = total.toFixed(2)
+        let data = resp.data.data
+        this.walletPlanned.statTitle = data.total.toFixed(2)
         this.getMonthBudget()
+        
+      }).catch((error) => {
+        console.error(error);
+      })
 
     },
     getMonthIncoming() {
-      axios.get(DOMAIN + "/api/stats/month-wallet/incoming").then((resp) => {
+      axios.get(DOMAIN + "/api/stats/incoming").then((resp) => {
         let data = resp.data.data
-        this.incoming.statTitle = data.total_month.toFixed(2)
-        this.incoming.statPercent = data.percend_different.toFixed(2)
-        this.incoming.statArrow = data.percend_different <= 0 ? "down" : "up"
-        this.incoming.statPercentColor = data.percend_different <= 0 ? "text-red-500" : "text-emerald-500"
+        this.incoming.statTitle = data.total.toFixed(2)
+        this.incoming.statPercent = data.percentage.toFixed(2)
+        this.incoming.statArrow = data.percentage <= 0 ? "down" : "up"
+        this.incoming.statPercentColor = data.percentage <= 0 ? "text-red-500" : "text-emerald-500"
         this.getMonthBudget()
 
       }).catch((error) => {
@@ -195,12 +152,12 @@ export default {
       })
     },
     getMonthexpenses() {
-      axios.get(DOMAIN + "/api/stats/month-wallet/expenses").then((resp) => {
+      axios.get(DOMAIN + "/api/stats/expenses").then((resp) => {
         let data = resp.data.data
-        this.expenses.statTitle = data.total_month.toFixed(2)
-        this.expenses.statPercent = data.percend_different.toFixed(2)
-        this.expenses.statArrow = data.percend_different < 0 ? "down" : "up"
-        this.expenses.statPercentColor = data.percend_different > 0 ? "text-red-500" : "text-emerald-500"
+        this.expenses.statTitle = data.total.toFixed(2)
+        this.expenses.statPercent = data.percentage.toFixed(2)
+        this.expenses.statArrow = data.percentage < 0 ? "down" : "up"
+        this.expenses.statPercentColor = data.percentage > 0 ? "text-red-500" : "text-emerald-500"
         this.getMonthBudget()
 
       }).catch((error) => {
@@ -218,57 +175,6 @@ export default {
       }).catch((error) => {
         console.error(error);
       })
-    },
-    getMonthBudget() {
-
-      let savingpercentage = parseFloat(this.incoming.statTitle) * SAVING_PERCENTAGE
-      savingpercentage = savingpercentage / 100
-      savingpercentage = this.incoming.statTitle - savingpercentage
-
-      let percentage = parseFloat(this.expenses.statTitle) / savingpercentage
-      percentage = percentage * 100
-      percentage = percentage * -1
-
-      if(isNaN(percentage)) {
-        percentage = 100
-      }
-
-      let total = savingpercentage + parseFloat(this.expenses.statTitle)
-      total = total - this.planned
-      
-      if(total < 0) {
-        total = 0
-        percentage = 100
-      }
-
-      this.budget.statTitle = total.toFixed(2)
-      this.budget.statPercent = percentage.toFixed(2)
-      this.budget.statArrow = this.budget.statTitle < 0 ? "down" : "up"
-      this.budget.statPercentColor = percentage > 100 ? "text-red-500" : "text-emerald-500"
-      let _this = this
-      setTimeout(function () { _this.getMonthSavings() }, 2000)
-
-
-    },
-    getMonthSavings() {
-
-      let savings = parseFloat(this.incoming.statTitle) + parseFloat(this.expenses.statTitle)
-      savings = savings - this.planned
-      savings = savings <= 0 ? 0 : savings
-      let percentage = 0
-      if (savings != 0) {
-        percentage = savings / parseFloat(this.incoming.statTitle)
-        percentage = percentage * 100
-      }
-
-      if(isNaN(percentage)) {
-        percentage = 100
-      }
-
-      this.savings.statTitle = savings.toFixed(2)
-      this.savings.statPercent = percentage.toFixed(2)
-      this.savings.statArrow = this.savings.statTitle <= 0 ? "down" : "up"
-      this.savings.statPercentColor = percentage <= 0 ? "text-red-500" : "text-emerald-500"
     }
   }
 };
